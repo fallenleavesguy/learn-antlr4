4fc6146057ea4d6a68ff13186d3eabeec891708b
fallenleavesguy <9261562+fallenleavesguy@users.noreply.github.com>
2025-09-08 16:20:28 +0800

    save

---
+++ tour/ExtractInterfaceTool.java
@@ -0,0 +1,25 @@
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+public class ExtractInterfaceTool {
+  public static void main(String[] args) throws Exception {
+    String inputFile = null;
+    if (args.length > 0) inputFile = args[0];
+
+    InputStream is = System.in;
+    if (inputFile != null) is = new FileInputStream(inputFile);
+
+    CharStream input = CharStreams.fromStream(is);
+    JavaLexer lexer = new JavaLexer(input);
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    JavaParser parser = new JavaParser(tokens);
+    ParseTree tree = parser.compilationUnit();
+
+    ParseTreeWalker walker = new ParseTreeWalker();
+    ExtractInterfaceListener extractor = new ExtractInterfaceListener(parser);
+    walker.walk(extractor, tree);
+  }
+}
